aee180c17df76ec17d4896ef580763b9abff627b
Colton Wolkins (Indicio work address) <colton@indicio.tech>
2021-11-17 11:34:46 -0700

    feat: Add event listener routing file
    Signed-off-by: Colton Wolkins (Indicio work address) <colton@indicio.tech>

---
+++ acapy_plugin_qa/routes.py
@@ -0,0 +1,15 @@
+import logging
+improt re
+
+from acapy_cloudagent.core.event_bus import Event, EventBus
+from acapy_cloudagent.core.profile import Profile
+from acapy_cloudagent.messaging.responder import Base Responder
+
+from acapy_cloudagent.storage.error import StorageError, StorageNotFoundError
+
+LOGGER = getLogger(__name__)
+
+def register_events(event_bus: EventBus):
+	"""Register to handle events."""
+	pass
+
