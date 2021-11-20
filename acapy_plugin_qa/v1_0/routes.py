b5632c67b2c805d294183122b7663aa0420bc33f
Char Howland <char@indicio.tech>
2021-11-20 13:27:27 -0700

    feat: (WIP) add delegation to event handlers
    Signed-off-by: Char Howland <char@indicio.tech>

--- acapy_plugin_qa/v1_0/routes.py
+++ acapy_plugin_qa/v1_0/routes.py
@@ -6,7 +6,8 @@ from aries_cloudagent.core.event_bus import Event, EventBus
 from aries_cloudagent.core.profile import Profile
 from aries_cloudagent.messaging.responder import BaseResponder
 
-from aries_cloudagent.storage.error import StorageError, StorageNotFoundError
+from .messages.question import Question
+from .messages.answer import Answer
 
 from aiohttp_apispec import docs, request_schema, response_schema
 from .message_types import SPEC_URI
@@ -17,58 +18,76 @@ import uuid
 
 LOGGER = logging.getLogger(__name__)
 
+
 def register_events(event_bus: EventBus):
-	"""Register to handle events."""
-	event_bus.subscribe(
-		re.compile(f"^{QA_EVENT_PREFIX_PLACEHOLDER}{QUESTION_RECEIVED_EVENT_PLACEHOLDER}.*"),
-		on_question_received
-	)
-	event_bus.subscribe(
-		re.compile(f"^{QA_EVENT_PREFIX_PLACEHOLDER}{ANSWER_RECEIVED_EVENT_PLACEHOLDER}.*"),
-		on_answer_received
-	)
-	# When delegating, event subscriber prepares another
-	# question message and sends to delegated connection
-
-
-	# need to register to our question and answer events
-	# the cases of delegation
-	# see revocation routes.py for example below
-	"""
-	event_bus.subscribe(
-        re.compile(f"^{REVOCATION_EVENT_PREFIX}{REVOCATION_PUBLISHED_EVENT}.*"),
-        on_revocation_published,
+    """Register to handle events."""
+    event_bus.subscribe(
+        re.compile(f"^{QA_EVENT_PREFIX}{QUESTION_RECEIVED_EVENT}.*"),
+        on_question_received,
     )
     event_bus.subscribe(
-        re.compile(f"^{REVOCATION_EVENT_PREFIX}{REVOCATION_CLEAR_PENDING_EVENT}.*"),
-        on_pending_cleared,
-    )"""
+        re.compile(f"^{QA_EVENT_PREFIX}{ANSWER_RECEIVED_EVENT}.*"),
+        on_answer_received,
+    )
+
 
 async def on_question_received(profile: Profile, event: Event):
-	"""Handle question received event."""
-	# When delegating, event subscriber prepares another
-	# question message and sends to delegated connection
+    """Handle question received event."""
 
+    # check delegation
+    if not profile.settings.get("plugin_config", {}).get("qa", {}).get("delegate"):
+        return
 
+    # check to whom to delegate
+    key = profile.settings.get("plugin_config", {}).get("qa", {}).get("metadata_key")
+    value = (
+        profile.settings.get("plugin_config", {}).get("qa", {}).get("metadata_value")
+    )
 
-    # LOGGER.debug("Sending notification of revocation to recipient: %s", event.payload)
+    # TODO: create proper metadata query
+    # TODO: allow for returning more than one connection
+    if key == "role" and value == "admin":
+        thread_id = event.payload["@id"]
+        question_text = event.payload["question_text"]
+        question_detail = event.payload["question_detail"]
+        valid_responses = event.payload["valid_responses"]
 
-    # should_notify = profile.settings.get("revocation.notify", False)
-    # responder = profile.inject(BaseResponder)
-    # crids = event.payload.get("crids") or []
-
+        rewrapped_question = Question(
+            thread_id,
+            question_text,
+            question_detail,
+            valid_responses,
+        )
+        responder = profile.inject(BaseResponder)
+        await responder.send(rewrapped_question, connection_id=connection_id)
 
 
 async def on_answer_received(profile: Profile, event: Event):
-	"""Handle answer received event."""
+    """Handle answer received event."""
 
+    # check delegation
+    if not profile.settings.get("plugin_config", {}).get("qa", {}).get("delegate"):
+        return
 
+    # check pthid
+    if not event.payload["pthid"]:
+        return
 
+    # TODO: properly lookup QAExchangeRecord by thread_id == pthid:
+    if qa_exchange_record.pthid == event.payload["@id"]:
+        thread_id = event.payload["@id"]
+        pthid = event.payload["thread_id"]
+        response = event.payload["response"]
 
-
-
-
-
+        rewrapped_answer = Answer(
+            thread_id,
+            pthid,
+            response,
+        )
+        responder = profile.inject(BaseResponder)
+        await responder.send(
+            rewrapped_answer, connection_id=qa_exchange_record.connection_id
+        )
 
 
 # async def test_route(request: web.Request):
