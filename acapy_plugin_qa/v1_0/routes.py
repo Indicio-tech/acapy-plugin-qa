edc0138521e942eded3e5dfc9ed8837f158a4039
Colton Wolkins (Indicio work address) <colton@indicio.tech>
2021-11-22 07:36:38 -0700

    feat: Setup Admin API for sending questions to a connection
    Signed-off-by: Colton Wolkins (Indicio work address) <colton@indicio.tech>

--- acapy_plugin_qa/v1_0/routes.py
+++ acapy_plugin_qa/v1_0/routes.py
@@ -8,11 +8,14 @@ from aries_cloudagent.messaging.responder import BaseResponder
 
 from aries_cloudagent.storage.error import StorageError, StorageNotFoundError
 
-from aiohttp_apispec import docs, request_schema, response_schema
+from aiohttp_apispec import docs, request_schema, response_schema, match_info_schema
 from .message_types import SPEC_URI
 from aries_cloudagent.messaging.agent_message import AgentMessage, AgentMessageSchema
-from .messages.question import Question
+from .messages.question import Question, QuestionSchema
 from marshmallow import fields, ValidationError, pre_dump
+from aries_cloudagent.messaging.models.openapi import OpenAPISchema
+from aries_cloudagent.messaging.valid import UUIDFour
+from aries_cloudagent.connections.models.conn_record import ConnRecord
 import uuid
 
 LOGGER = logging.getLogger(__name__)
@@ -28,7 +31,7 @@ def register_events(event_bus: EventBus):
 # 	app.add_routes([web.get("/qa", test_route)])
 
 
-class QuestionSchema(AgentMessageSchema):
+class QuestionRequestSchema(AgentMessageSchema):
     """Schema for Question message."""
 
     class Meta:
@@ -49,13 +52,23 @@ class QuestionSchema(AgentMessageSchema):
             "A list of dictionaries indicating possible valid responses to the question."
         ),
     )
+
+
+class BasicConnIdMatchInfoSchema(OpenAPISchema):
+    """Path parameters and validators for request taking connection id."""
+
+    conn_id = fields.Str(
+        description="Connection identifier", required=True, example=UUIDFour.EXAMPLE
+    )
+
 @docs(
     tags=["QAProtocol"],
     summary="Question & Answer Protocol",
 )
+@match_info_schema(BasicConnIdMatchInfoSchema())
 @request_schema(QuestionSchema())
 @response_schema(QuestionSchema(), 200, description="")
-async def qa_features(request: web.BaseRequest):
+async def send_question(request: web.BaseRequest):
     """
     Request handler for inspecting supported protocols.
 
@@ -66,26 +79,50 @@ async def qa_features(request: web.BaseRequest):
         The diclosed protocols response
 
     """
+    # Extract question data sent to us (the Questioner)
     context: AdminRequestContext = request["context"]
+    connection_id = request.match_info["conn_id"]
+    outbound_handler = request["outbound_message_router"]
+    params = await request.json()
     print(request)
     # registry: ProtocolRegistry = context.inject(ProtocolRegistry)
     # results = registry.protocols_matching_query(request.query.get("query", "*"))
-    results = request.query.get("question_text")
-    q = Question(
-    	question_text=request.query.get("question_text"),
-    	valid_responses={"valid_responses": {"text": k} for k in request.query.getall("valid_responses") }
-    	# {"results": {k: {} for k in results}}
-    	)
+    # results = request.query.get("question_text")
+    # q = Question(
+    # 	question_text=request.query.get("question_text"),
+    # 	valid_responses={"valid_responses": {"text": k} for k in request.query.getall("valid_responses") }
+    # 	# {"results": {k: {} for k in results}}
+    # 	)
     # q.assign_thread_id(request.query.get("@id") or uuid.uuid4())
-    q.assign_thread_from(q)
+    # q.assign_thread_from(q)
 
-    return web.json_response(q.serialize())
+
+    try:
+        async with context.session() as session:
+            connection = await ConnRecord.retrieve_by_id(session, connection_id)
+    except StorageNotFoundError as err:
+        raise web.HTTPNotFound(reason=err.roll_up) from err
+
+    if connection.is_ready:
+        # Setup a question object to pass on to the responder
+        msg = Question(
+            _id=params["@id"],
+            question_text=params["question_text"],
+            question_detail=params["question_detail"],
+            valid_responses=params["valid_responses"]
+        )
+        msg.assign_thread_id(params["@id"])  # At this time, the thid is required for serialization
+        await outbound_handler(msg, connection_id=connection_id)
+
+    return web.json_response({})
+
+    return web.json_response(msg.serialize())
 
 
 async def register(app: web.Application):
     """Register routes."""
 
-    app.add_routes([web.post("/qa", qa_features)])
+    app.add_routes([web.post("/qa/{conn_id}/send-question", send_question)])
 
 
 def post_process_routes(app: web.Application):
