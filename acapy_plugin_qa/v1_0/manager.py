c7cd3d1899c2026fab43ee1006452d9fc9073519
Colton Wolkins (Indicio work address) <colton@indicio.tech>
2021-11-22 21:21:33 -0700

    feat: QAManager can now create and store exchange records
    Signed-off-by: Colton Wolkins (Indicio work address) <colton@indicio.tech>

--- acapy_plugin_qa/v1_0/manager.py
+++ acapy_plugin_qa/v1_0/manager.py
@@ -1,6 +1,8 @@
 import logging
 from aries_cloudagent.core.profile import Profile
 from aries_cloudagent.core.error import BaseError
+from .messages.question import Question
+from .models.qa_exchange_record import QAExchangeRecord
 
 
 class QAManagerError(BaseError):
@@ -15,8 +17,23 @@ class QAManager:
         self.profile = profile
         self.logger = logging.getLogger(__name__)
 
-    async def answer_question():
+    async def answer_question(self):
         pass
 
-    async def store_question():
-        pass
+    async def store_question(self, question: Question, connection_id: str):
+        qa_exchange_record = QAExchangeRecord(
+            # question_answer_id = question._id,
+            thread_id = question._id,
+            # thread_id = question._thread.thid if question._thread else question.id,
+            # pthid = None,
+            connection_id = connection_id,
+            valid_responses = question.valid_responses,
+            question_text = question.question_text,
+            question_detail = question.question_detail
+        )
+
+        async with self.profile.session() as session:
+            await qa_exchange_record.save(session)
+
+    async def retrieve_by_id(self, session, thread_id):
+        return await QAExchangeRecord.retrieve_by_id(session, thread_id)
